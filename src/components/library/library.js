30b7b860a67e19c170bb3ccd77e6f434d25a0418
Tanner Noble <tannernoble@Tanners-iMac.local>
2019-12-20 15:24:52 -0600

    build library and schedule components and put them on the grid

---
+++ src/components/library/library.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Library extends Component {
+  render() {
+    return (
+      <div className="library">
+        <h1 className="library__title">Course library</h1>
+      </div>
+    );
+  }
+}
