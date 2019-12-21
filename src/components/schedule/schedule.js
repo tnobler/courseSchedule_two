c02cef9f9f84d7e9f9ff4c61c73975fb0857af3e
Tanner Noble <tannernoble@Tanners-iMac.local>
2019-12-21 15:25:51 -0600

    built progress tracker component

--- src/components/schedule/schedule.js
+++ src/components/schedule/schedule.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Gradient from '../gradient';
 import ScheduleCourse from './scheduleCourse';
+import ProgressTracker from './progressTracker';
 
 export default class Schedule extends Component {
   render() {
@@ -11,6 +12,7 @@ export default class Schedule extends Component {
         <ScheduleCourse title={'Problem Solving'} />
         <ScheduleCourse title={'Problem Solving'} />
         <ScheduleCourse title={'Problem Solving'} />
+        <ProgressTracker />
         <Gradient />
       </div>
     );
