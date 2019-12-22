7bcb863e46685e9286c385c517f75c436c73eb18
Tanner Noble <tannernoble@Tanners-iMac.local>
2019-12-21 20:42:24 -0600

    mapped course object data into library course component instances

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -9,7 +9,7 @@ class LibraryCourse extends Component {
     return (
       <div className="library-course">
         <div className="library-course__title-check">
-          <label className="library-course__title">Problem Solving</label>
+          <label className="library-course__title">{ this.props.title }</label>
           { Icon('fas fa-check', 'library-course__icon') }
         </div>
         <Arrow className="library-course__arrow" />
@@ -17,7 +17,7 @@ class LibraryCourse extends Component {
 
         <div className="library-course__description">
           <label>Course Description</label>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pellentesque lectus ac dolor mollis, sed feugiat ante ornare. Aliquam interdum ornare sollicitudin. Sed non ligula pharetra, dapibus magna vel, egestas neque.</p>
+          <p>{this.props.description}</p>
         </div>
       </div>
     );
