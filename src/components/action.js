2910c3976fee0fc536b36345f475b54116f7e4ef
Tanner Noble <tannernoble@Tanners-iMac.local>
2020-01-01 15:34:59 -0600

    styled schedule course component and refactored schedule grid template rows

--- src/components/action.js
+++ src/components/action.js
@@ -29,11 +29,5 @@ export default class Action extends Component {
         
       </a>
     );
-    // return (
-    //   <button onClick={() => this.props.onClick()} className={`${this.props.className} action`}>
-    //     { Icon('fas fa-plus-circle') }
-    //     {/* { Icon('fas fa-times-circle') } */}
-    //   </button>
-    // );
   }
 }
